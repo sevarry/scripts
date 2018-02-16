b53cc4345ef167dc9f0e3656903b0a6257c600e6
sevarry <ted.eckerman@gmail.com>
2018-02-15 23:20:47 -0800

    Create logcheck.py

---
+++ logcheck.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+log = 'log.txt'
+fails = []
+
+with open(log) as f:
+    for line in f:
+        if 'failed login' in line:
+            name = line.partition(' ')[0]
+            fails.append(name)
+
+cnt = Counter(fails)
+inc = [k for k, v in cnt.iteritems() if v >= 3]
+
+print
+print 'The following users have failed login 3 or more times:'
+for i in inc:
+    print
+    print 'username: ', i
+print
